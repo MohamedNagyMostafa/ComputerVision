5826f2c219638bba1ee96c4776dfc82ee0c82d51
M <mohamednagy2015@outlook.com>
2019-08-28 14:56:56 +0200

    display the progress

--- Normalization Correlation/main.m
+++ Normalization Correlation/main.m
@@ -6,7 +6,14 @@ img_face_clip = img(74:137, 186:243);
 [indX indY] = find_template_2D(img_face_clip, img)
 
 [y_s x_s] = size(img_face_clip);
-
+subplot(1,3,1);
 imshow(img);
+title('original');
+subplot(1,3,2);
+imshow(img_face_clip);
+title('template');
+subplot(1,3,3);
+imshow(img);
+title('After Norm. Corr.');
 hold on;
 rectangle('Position', [indY indX y_s x_s],'EdgeColor', 'b','LineWidth', 3 ); 
